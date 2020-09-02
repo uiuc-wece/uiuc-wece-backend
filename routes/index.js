5486dbb19365c309d5fb2952c16616da93dd471f
Polarpi <eugeniachen123@gmail.com>
2020-09-02 13:42:45 -0500

    add mailerlite routes for adding subscriber + unsubscribe

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
-var express = require("express");
-var router = express.Router();
+import express from "express";
+const router = express.Router();
 
-var user = require("./user");
-var event = require("./event");
+import { getUser, createUser, updateUser, deleteUser } from "./user.js";
+import { getEvent, createEvent, updateEvent, deleteEvent } from "./event.js";
+import { addSubscriber, unsubscribe } from "./mailerlite.js";
 
-router.get("/user/:netId", user.getUser);
-router.post("/user", user.createUser);
-router.put("/user/:netId", user.updateUser);
-router.delete("/user/:netId", user.deleteUser);
+router.get("/user/:netId", getUser);
+router.post("/user", createUser);
+router.put("/user/:netId", updateUser);
+router.delete("/user/:netId", deleteUser);
 
-router.get("/event/:eventKey", event.getEvent);
-router.post("/event", event.createEvent);
-router.put("/event/:eventKey", event.updateEvent);
-router.delete("/event/:eventKey", event.deleteEvent);
+router.get("/event/:eventKey", getEvent);
+router.post("/event", createEvent);
+router.put("/event/:eventKey", updateEvent);
+router.delete("/event/:eventKey", deleteEvent);
 
-module.exports = router;
+router.post("/subscriber", addSubscriber);
+router.put("/subscriber", unsubscribe);
+
+export default router;
